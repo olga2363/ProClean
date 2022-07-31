6727bd5be225d830069dc21bd277ce635cd72840
olga2363 <69006206+olga2363@users.noreply.github.com>
2022-07-31 21:54:48 +0300

    change css delete

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $('a[href*="#"]').on('click', function (e) {
    
     $('html, body').animate({
       scrollTop: $($(this).attr('href')).offset().top
-    }, 4000, 'linear');
+    }, 2000, 'linear');
   });
 
 $(function () {
