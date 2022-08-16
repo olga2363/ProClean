30cedc0a96c3edb8f67e15bb632d117dc9293d8a
olga2363 <69006206+olga2363@users.noreply.github.com>
2022-08-16 12:40:34 +0300

    logo size

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navigation").style.top = "0";
   } else {
-    document.getElementById("navigation").style.top = "-130px";
+    document.getElementById("navigation").style.top = "-120px";
   }
   prevScrollpos = currentScrollPos;
 }
